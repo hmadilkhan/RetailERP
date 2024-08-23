c148134af226939584ea58ea50a676aa6f53d4f5
hmadilkhan <adilsziu@outlook.com>
2024-08-23 15:59:25 +0500

    Libs updated

--- resources/views/partials/html-libs.blade.php
+++ resources/views/partials/html-libs.blade.php
@@ -1,96 +1,96 @@
-    <script src="{{ asset('public/js/raphael-min.js')}}"></script>
-    <script src="{{ asset('public/js/jquery-1.8.2.min.js')}}"></script>
-    <script src="{{ asset('public/components/morris.js/morris.js')}}"></script>  
-     <script src="{{asset('public/components/chart.js/dist/Chart.js')}}"></script>   
-   <!-- Favicon icon
-   <link rel="shortcut icon" href="{{ asset('public/assets/images/favicon.jpg') }}" type="image/x-icon">-->
-    <link rel="icon" href="{{ asset('public/assets/images/favicon.ico') }}" type="image/x-icon"> 
+<script src="{{ asset('js/raphael-min.js')}}"></script>
+<script src="{{ asset('js/jquery-1.8.2.min.js')}}"></script>
+<script src="{{ asset('components/morris.js/morris.js')}}"></script>  
+ <script src="{{asset('components/chart.js/dist/Chart.js')}}"></script>   
+<!-- Favicon icon
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.jpg') }}" type="image/x-icon">-->
+<link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon"> 
 
-   <!-- Google font-->
-   <link href="{{ asset('public/assets/css6ff6.css?family=Ubuntu:400,500,700') }}" rel="stylesheet">
+<!-- Google font-->
+<link href="{{ asset('assets/css6ff6.css?family=Ubuntu:400,500,700') }}" rel="stylesheet">
 
-   <!--Date Picker Material Icon Css-->
-   <link href="{{ asset('public/assets/icone91f.css?family=Material+Icons') }}" rel="stylesheet">
+<!--Date Picker Material Icon Css-->
+<link href="{{ asset('assets/icone91f.css?family=Material+Icons') }}" rel="stylesheet">
 
-   <!-- themify -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/icon/themify-icons/themify-icons.css') }}">
+<!-- themify -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/themify-icons/themify-icons.css') }}">
 
-   <!-- iconfont -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/icon/icofont/css/icofont.css') }}">
-
-
-   <!-- simple line icon -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/icon/simple-line-icons/css/simple-line-icons.css') }}">
-
-   <!-- Required Fremwork -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/components/bootstrap/dist/css/bootstrap.min.css') }}">
-
-   <!-- Chartlist chart css -->
-   <link rel="stylesheet" href="{{ asset('public/components/chartist/dist/chartist.css') }}" type="text/css" media="all">
-
-   <!-- Weather css -->
-   <link href="{{ asset('public/assets/css/svg-weather.css') }}" rel="stylesheet">
+<!-- iconfont -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/icofont/css/icofont.css') }}">
 
 
-   <!-- Style.css -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/main.css') }}">
-   
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/menu.css')}}">
+<!-- simple line icon -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/icon/simple-line-icons/css/simple-line-icons.css') }}">
 
-   <!-- Responsive.css-->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/css/responsive.css') }}">
+<!-- Required Fremwork -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/bootstrap/dist/css/bootstrap.min.css') }}">
 
-   	<!-- Date Picker css -->
-	<link rel="stylesheet" href="{{ asset('public/components/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" />
+<!-- Chartlist chart css -->
+<link rel="stylesheet" href="{{ asset('components/chartist/dist/chartist.css') }}" type="text/css" media="all">
 
-	<!-- Bootstrap Date-Picker css -->
-	<link rel="stylesheet" href="{{ asset('public/assets/plugins/bootstrap-datepicker/css/bootstrap-datetimepicker.css') }}" />
-	<link rel="stylesheet" type="text/css" href="{{ asset('public/components/bootstrap-daterangepicker/daterangepicker.css') }}" />
-
-	<!-- Select 2 css -->
-	<link rel="stylesheet" href="{{ asset('public/components/select2/dist/css/select2.min.css') }}" />
-	<link rel="stylesheet" type="text/css" href="{{ asset('public/assets/plugins/select2/css/s2-docs.css') }}">
-
-	<!-- Multi Select css -->
-	<link rel="stylesheet" href="{{ asset('public/components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css') }}" />
-	<link rel="stylesheet" href="{{ asset('public/components/multiselect/css/multi-select.css') }}" />
-
-	<!-- Tags css -->
-	<link rel="stylesheet" href="{{ asset('public/components/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}" />
-
-	<!-- Switchery css -->
-	<link rel="stylesheet" type="text/css" href="{{ asset('public/components/switchery/dist/switchery.min.css') }}">
-
-   <!-- Dropzone css -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/components/dropzone/dist/dropzone.css') }}">
-
-      <!-- Data Table Css -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/components/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/plugins/data-table/css/buttons.dataTables.min.css') }}">
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}">
-
-    <!-- Light Box css -->
-  <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/plugins/light-box/css/ekko-lightbox.css')}}">
-
-  <!-- Light Box 2 css -->
-  <link rel="stylesheet" type="text/css" href="{{ asset('public/components/lightbox2/dist/css/lightbox.css')}}">
-
-      <!-- Modal animation CSS -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/plugins/modal/css/component.css') }}">
-
-      <!-- Sweetalert CSS -->
-   <link rel="stylesheet" type="text/css" href="{{ asset('public/assets/plugins/sweetalert/css/sweetalert.css') }}">
+<!-- Weather css -->
+<link href="{{ asset('assets/css/svg-weather.css') }}" rel="stylesheet">
 
 
-    <link type="text/css" rel="stylesheet" href="{{ asset('public/components/syntaxhighlighter/styles/shCoreDjango.css') }}" />
+<!-- Style.css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/main.css') }}">
 
-   
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/menu.css')}}">
 
-    <!-- carousel css -->
-    <link href="{{ asset('public/assets/css/carousel.min.css') }}" rel="stylesheet">
-   
-   <!-- owl carousel css -->
-    <link href="{{ asset('public/assets/css/owl.carousel.css') }}" rel="stylesheet">
+<!-- Responsive.css-->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
 
-   <!-- swiper css -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('public/components/swiper/dist/css/swiper.min.css') }}">
+  <!-- Date Picker css -->
+<link rel="stylesheet" href="{{ asset('components/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css') }}" />
+
+<!-- Bootstrap Date-Picker css -->
+<link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-datepicker/css/bootstrap-datetimepicker.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('components/bootstrap-daterangepicker/daterangepicker.css') }}" />
+
+<!-- Select 2 css -->
+<link rel="stylesheet" href="{{ asset('components/select2/dist/css/select2.min.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/select2/css/s2-docs.css') }}">
+
+<!-- Multi Select css -->
+<link rel="stylesheet" href="{{ asset('components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css') }}" />
+<link rel="stylesheet" href="{{ asset('components/multiselect/css/multi-select.css') }}" />
+
+<!-- Tags css -->
+<link rel="stylesheet" href="{{ asset('components/bootstrap-tagsinput/dist/bootstrap-tagsinput.css') }}" />
+
+<!-- Switchery css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/switchery/dist/switchery.min.css') }}">
+
+<!-- Dropzone css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/dropzone/dist/dropzone.css') }}">
+
+  <!-- Data Table Css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/data-table/css/buttons.dataTables.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('components/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}">
+
+<!-- Light Box css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/light-box/css/ekko-lightbox.css')}}">
+
+<!-- Light Box 2 css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/lightbox2/dist/css/lightbox.css')}}">
+
+  <!-- Modal animation CSS -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/modal/css/component.css') }}">
+
+  <!-- Sweetalert CSS -->
+<link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/sweetalert/css/sweetalert.css') }}">
+
+
+<link type="text/css" rel="stylesheet" href="{{ asset('components/syntaxhighlighter/styles/shCoreDjango.css') }}" />
+
+
+
+<!-- carousel css -->
+<link href="{{ asset('assets/css/carousel.min.css') }}" rel="stylesheet">
+
+<!-- owl carousel css -->
+<link href="{{ asset('assets/css/owl.carousel.css') }}" rel="stylesheet">
+
+<!-- swiper css -->
+<link rel="stylesheet" type="text/css" href="{{ asset('components/swiper/dist/css/swiper.min.css') }}">
--- resources/views/partials/js-libs.blade.php
+++ resources/views/partials/js-libs.blade.php
@@ -1,122 +1,122 @@
    <!-- Required Jqurey -->
-   <script src="{{ asset('public/components/Jquery/dist/jquery.min.js') }}"></script>
-   <script src="{{ asset('public/components/jquery-ui/jquery-ui.min.js') }}"></script>
-   <script src="{{ asset('public/components/tether/dist/js/tether.min.js') }}"></script>
+   <script src="{{ asset('components/Jquery/dist/jquery.min.js') }}"></script>
+   <script src="{{ asset('components/jquery-ui/jquery-ui.min.js') }}"></script>
+   <script src="{{ asset('components/tether/dist/js/tether.min.js') }}"></script>
 
    <!-- Required Fremwork -->
-   <script src="{{ asset('public/components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+   <script src="{{ asset('components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
    <!-- Scrollbar JS-->
-   <script src="{{ asset('public/components/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
-   <script src="{{ asset('public/components/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+   <script src="{{ asset('components/jquery-slimscroll/jquery.slimscroll.js') }}"></script>
+   <script src="{{ asset('components/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
 
    <!--classic JS-->
-   <script src="{{ asset('public/components/classie/classie.js') }}"></script>
+   <script src="{{ asset('components/classie/classie.js') }}"></script>
 
    <!-- notification -->
-   <script src="{{ asset('public/assets/plugins/notification/js/bootstrap-growl.min.js') }}"></script>
+   <script src="{{ asset('assets/plugins/notification/js/bootstrap-growl.min.js') }}"></script>
    <!-- notification js -->
-   <script src="{{ asset('public/js/notification.js') }}" type="text/javascript"></script>
+   <script src="{{ asset('js/notification.js') }}" type="text/javascript"></script>
 
    <!-- Sparkline charts -->
-   <script src="{{ asset('public/components/jquery-sparkline/dist/jquery.sparkline.js') }}"></script>
+   <script src="{{ asset('components/jquery-sparkline/dist/jquery.sparkline.js') }}"></script>
 
    <!-- Counter js  -->
-   <script src="{{ asset('public/components/waypoints/lib/jquery.waypoints.min.js') }}"></script>
-   <script src="{{ asset('public/assets/plugins/countdown/js/jquery.counterup.js') }}"></script>
+   <script src="{{ asset('components/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+   <script src="{{ asset('assets/plugins/countdown/js/jquery.counterup.js') }}"></script>
 
    <!-- Echart js -->
-  <!--  <script src="{{ asset('public/assets/plugins/charts/echarts/js/echarts-all.js') }}"></script> -->
+  <!--  <script src="{{ asset('assets/plugins/charts/echarts/js/echarts-all.js') }}"></script> -->
 
-   <!--<script src="{{ asset('public/assets/highcharts.js') }}"></script>
-   <script src="{{ asset('public/assets/modules/exporting.js') }}"></script>
-   <script src="{{ asset('public/assets/highcharts-3d.js') }}"></script>-->
+   <!--<script src="{{ asset('assets/highcharts.js') }}"></script>
+   <script src="{{ asset('assets/modules/exporting.js') }}"></script>
+   <script src="{{ asset('assets/highcharts-3d.js') }}"></script>-->
 
    <!-- custom js -->
-   <script type="text/javascript" src="{{ asset('public/assets/js/main.min.js') }}"></script>
-   <script type="text/javascript" src="{{ asset('public/assets/js/menu-horizontal.min.js') }}"></script>
-  <!--  <script type="text/javascript" src="{{ asset('public/assets/pages/dashboard.js') }}"></script> -->
-   <script type="text/javascript" src="{{ asset('public/assets/pages/accordion.js') }}"></script>
-   <script type="text/javascript" src="{{ asset('public/assets/pages/elements.js') }}"></script>
-   <script src="{{ asset('public/assets/js/menu.min.js') }}"></script>
+   <script type="text/javascript" src="{{ asset('assets/js/main.min.js') }}"></script>
+   <script type="text/javascript" src="{{ asset('assets/js/menu-horizontal.min.js') }}"></script>
+  <!--  <script type="text/javascript" src="{{ asset('assets/pages/dashboard.js') }}"></script> -->
+   <script type="text/javascript" src="{{ asset('assets/pages/accordion.js') }}"></script>
+   <script type="text/javascript" src="{{ asset('assets/pages/elements.js') }}"></script>
+   <script src="{{ asset('assets/js/menu.min.js') }}"></script>
 
    	<!-- Date picker.js-->
-	<script src="{{ asset('public/assets/plugins/datepicker/js/moment-with-locales.min.js') }}"></script>
-	<script src="{{ asset('public/components/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script> 
+	<script src="{{ asset('assets/plugins/datepicker/js/moment-with-locales.min.js') }}"></script>
+	<script src="{{ asset('components/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script> 
 
 	<!-- Max-Length js --> 
-	<script src="{{ asset('public/components/bootstrap-maxlength/src/bootstrap-maxlength.js') }}"></script>
+	<script src="{{ asset('components/bootstrap-maxlength/src/bootstrap-maxlength.js') }}"></script>
 
 	<!-- switchery js --> 
-	<script type="text/javascript" src="{{ asset('public/components/switchery/dist/switchery.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('components/switchery/dist/switchery.min.js') }}"></script>
 
 	<!-- Select 2 js -->
-	<script src="{{ asset('public/components/select2/dist/js/select2.full.min.js') }}"></script> 
+	<script src="{{ asset('components/select2/dist/js/select2.full.min.js') }}"></script> 
 		<!-- Multi Select js -->
-	<script src="{{ asset('public/components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js') }}"></script>
-	<script src="{{ asset('public/components/multiselect/js/jquery.multi-select.js') }}"></script>
-	<script type="text/javascript" src="{{ asset('public/assets/plugins/multi-select/js/jquery.quicksearch.js') }}"></script>
+	<script src="{{ asset('components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js') }}"></script>
+	<script src="{{ asset('components/multiselect/js/jquery.multi-select.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('assets/plugins/multi-select/js/jquery.quicksearch.js') }}"></script>
 
 	<!-- Tags js -->
-	<script src="{{ asset('public/components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js') }}"></script>
+	<script src="{{ asset('components/bootstrap-tagsinput/dist/bootstrap-tagsinput.js') }}"></script>
 
 	<!-- Bootstrap Datepicker js -->
-	<script type="text/javascript" src="{{ asset('public/components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
-	<script src="{{ asset('public/assets/plugins/bootstrap-datepicker/js/bootstrap-datetimepicker.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/bootstrap-datepicker/js/bootstrap-datetimepicker.min.js') }}"></script>
 
       <!-- Date picker.js -->
-   <script src="{{asset('public/assets/plugins/datepicker/js/moment-with-locales.min.js')}}"></script>
-   <script src="{{ asset('public/components/moment/moment.js') }}"></script>
-   <script src="{{ asset('public/components/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script>
+   <script src="{{asset('assets/plugins/datepicker/js/moment-with-locales.min.js')}}"></script>
+   <script src="{{ asset('components/moment/moment.js') }}"></script>
+   <script src="{{ asset('components/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js') }}"></script>
 
       
    
 
 	<!-- bootstrap range picker -->
-	<script type="text/javascript" src="{{ asset('public/components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('components/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
 
       <!-- Dropzone js -->
-   <script src="{{ asset('public/components/dropzone/dist/dropzone.js') }}"></script>
+   <script src="{{ asset('components/dropzone/dist/dropzone.js') }}"></script>
 
       <!-- data-table js -->
-   <script src="{{ asset('public/components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-   <script src="{{ asset('public/assets/plugins/data-table/js/jszip.min.js') }}"></script>
-   <script src="{{ asset('public/assets/plugins/data-table/js/pdfmake.min.js') }}"></script>
-   <script src="{{ asset('public/assets/plugins/data-table/js/vfs_fonts.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-   <script src="{{ asset('public/components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
+   <script src="{{ asset('assets/plugins/data-table/js/jszip.min.js') }}"></script>
+   <script src="{{ asset('assets/plugins/data-table/js/pdfmake.min.js') }}"></script>
+   <script src="{{ asset('assets/plugins/data-table/js/vfs_fonts.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+   <script src="{{ asset('components/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
       <!-- Model animation js -->
-   <script src="{{ asset('public/components/classie/classie.js') }}"></script>
-   <script src="{{ asset('public/components/ModalWindowEffects/js/modalEffects.js') }}"></script>
+   <script src="{{ asset('components/classie/classie.js') }}"></script>
+   <script src="{{ asset('components/ModalWindowEffects/js/modalEffects.js') }}"></script>
    <!-- swiper js -->
-   <script src="{{ asset('public/components/swiper/dist/js/swiper.min.js')}}"></script>
+   <script src="{{ asset('components/swiper/dist/js/swiper.min.js')}}"></script>
 
    <!-- owl carousel 2 js -->
-   <script src="{{ asset('public/assets/js/owl.min.js') }}"></script>
+   <script src="{{ asset('assets/js/owl.min.js') }}"></script>
 
-   <script src="{{ asset('public/assets/plugins/light-box/js/ekko-lightbox.js') }}"></script>
-   <script src="{{ asset('public/components//lightbox2/dist/js/lightbox.js')}}"></script>
+   <script src="{{ asset('assets/plugins/light-box/js/ekko-lightbox.js') }}"></script>
+   <script src="{{ asset('components//lightbox2/dist/js/lightbox.js')}}"></script>
    
     <!-- Slider -->
-    <script type="text/javascript" src="{{ asset('public/assets/pages/slider.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/pages/slider.js') }}"></script>
 
       <!-- Sweetalert js -->
-   <script src="{{ asset('public/assets/plugins/sweetalert/js/sweetalert.js') }}"></script>
+   <script src="{{ asset('assets/plugins/sweetalert/js/sweetalert.js') }}"></script>
 
-   <script src="{{ asset('public/assets/pages/data-table.js') }}"></script>
+   <script src="{{ asset('assets/pages/data-table.js') }}"></script>
 
 
-   <script src="{{ asset('public/assets/js/custom-js.js') }}"></script>
+   <script src="{{ asset('assets/js/custom-js.js') }}"></script>
 
     <!-- Highliter js -->
-   <script type="text/javascript" src="{{ asset('public/components/syntaxhighlighter/scripts/shCore.js')}}"></script>
-   <script type="text/javascript" src="{{ asset('public/components/syntaxhighlighter/scripts/shBrushJScript.js')}}"></script>
-   <script type="text/javascript" src="{{ asset('public/components/syntaxhighlighter/scripts/shBrushXml.js')}}"></script>
+   <script type="text/javascript" src="{{ asset('components/syntaxhighlighter/scripts/shCore.js')}}"></script>
+   <script type="text/javascript" src="{{ asset('components/syntaxhighlighter/scripts/shBrushJScript.js')}}"></script>
+   <script type="text/javascript" src="{{ asset('components/syntaxhighlighter/scripts/shBrushXml.js')}}"></script>
    <script type="text/javascript">
       SyntaxHighlighter.all();
    </script>
