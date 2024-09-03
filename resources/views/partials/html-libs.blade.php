b8340a8634151fc6e3ca9f6ea585fa2f8ca081e9
hmadilkhan <adilsziu@outlook.com>
2024-09-03 16:22:51 +0500

    Changed

--- resources/views/partials/html-libs.blade.php
+++ resources/views/partials/html-libs.blade.php
@@ -3,9 +3,9 @@
     <script src="{{ asset('components/morris.js/morris.js') }}"></script>
     <script src="{{ asset('components/chart.js/dist/Chart.js') }}"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> --}}
     <!-- Favicon icon
    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.jpg') }}" type="image/x-icon">-->
     <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
