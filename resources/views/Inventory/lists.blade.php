9d3ec0b22361fcfa8cfc28b7035bb910f33549c5
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-03 15:47:07 +0500

    image working

--- resources/views/Inventory/lists.blade.php
+++ resources/views/Inventory/lists.blade.php
@@ -960,7 +960,7 @@
 				   columnDeal_btn = "<a href='/inventory/"+value.id+"/variable-products' class='m-r-1' title='Create Variable & Addon Product'><i class='icofont icofont-plus text-success'></i></a>"; 
 				}
                 
-                if($.inArray(value.company_id,[95, 102, 104,7]) || $.inArray(GlobalUsername,['demoadmin','fnkhan'])){
+                if($.inArray(value.company_id,[95, 102, 104,7])  != -1 || $.inArray(GlobalUsername,['demoadmin','fnkhan']) != -1){
                     
                     if(value.product_image_url != ''){
                         imageUrl = value.product_image_url;
