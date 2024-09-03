9055320d6697410da35977d3453a4c8c6fc51a04
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-03 15:23:57 +0500

    image working

--- resources/views/Inventory/lists.blade.php
+++ resources/views/Inventory/lists.blade.php
@@ -958,13 +958,13 @@
 				   columnDeal_btn = "<a href='/inventory/"+value.id+"/variable-products' class='m-r-1' title='Create Variable & Addon Product'><i class='icofont icofont-plus text-success'></i></a>"; 
 				}
 
-                //if($.inArray(value.company_id,[95, 102, 104]) || $.inArray(strtolower(Auth::user()->username),['demoadmin','fnkhan'])){
+                if($.inArray(value.company_id,[95, 102, 104])){
                     imageUrl = value.url;
-                // }else{
-                //     if(value.product_image != ''){
-                //      imageUrl = "storage/images/products/"+value.product_image;
-                //     }
-                // }
+                }else{
+                    if(value.product_image != ''){
+                     imageUrl = "storage/images/products/"+value.product_image;
+                    }
+                }
 				
 				$("#inventtbl tbody").append(
 					"<tr>"+
