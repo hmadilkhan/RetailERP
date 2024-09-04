48c2a632a8b6216ffe5fbe8defbe12ba0f579142
hmadilkhan <adilsziu@outlook.com>
2024-09-04 15:11:30 +0500

    Query

--- app/transfer.php
+++ app/transfer.php
@@ -220,7 +220,6 @@ WHERE a.DC_id = ?',[$dcid]);
         $result = DB::select('SELECT a.id, a.product_name,a.item_code FROM inventory_general a
 		INNER JOIN inventory_stock b ON b.product_id = a.id
 		WHERE b.branch_id = ?
-		GROUP BY a.product_name
 		ORDER BY a.id ASC',[$branchid]);
         return $result;
     }
