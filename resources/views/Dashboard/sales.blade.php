ff944c43d2f94e237bf4d3ad10a8efbf8cd21d04
hmadilkhan <adilsziu@outlook.com>
2024-09-04 10:37:57 +0500

    Sales Details Bootstrap 5

--- resources/views/Dashboard/sales.blade.php
+++ resources/views/Dashboard/sales.blade.php
@@ -7,6 +7,12 @@
 @section('navdashboard','active')
 
 @section('content')
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css"
+integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 <a href="{{ route('home') }}" class="btn btn-success text-white" ><i class="icofont icofont-arrow-left"></i>Back</a>
 
