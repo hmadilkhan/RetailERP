692eb6daff3c832ba8137b242749ea25e5488869
hmadilkhan <adilsziu@outlook.com>
2024-09-05 09:40:53 +0500

    Add User Relation  to ORderStatusLog

--- app/Models/OrderStatusLogs.php
+++ app/Models/OrderStatusLogs.php
@@ -7,17 +7,22 @@ use Illuminate\Support\Facades\DB;
 
 class OrderStatusLogs extends Model
 {
-	protected $table = "sales_online_order_status";
+    protected $table = "sales_online_order_status";
     protected $guarded = [];
     public $timestamps = false;
-	
-	public function status()
+
+    public function status()
     {
         return $this->belongsTo(OrderStatus::class, 'status_id', 'order_status_id');
     }
-	
-	public function branch()
+
+    public function branch()
     {
         return $this->belongsTo(Branch::class, 'branch_id', 'branch_id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 }
