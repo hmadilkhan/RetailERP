540c5f15753c238ffbe5b43b77786cbf1410c2bd
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-09 16:26:38 +0500

    working

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -1503,7 +1503,7 @@ class WebsiteController extends Controller
                 //$value = (array) $request->val;
                 $arrayValue = [];
                 array_push($arrayValue,$request->val);
-                $value = $arrayValue;
+                $value = json_encode($arrayValue);
 
             } else {
 
