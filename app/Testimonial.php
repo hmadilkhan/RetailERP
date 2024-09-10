d24cd4b03767365e538f2ef41762a4729dce8d62
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-10 12:53:26 +0500

    Testmonial model create

---
+++ app/Testimonial.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Company;
+
+class Testimonial extends Model
+{
+    protected $table = "website_testimonials";
+    protected $guarded = [];
+	
+
+}
