cc196e324cab9c4ccd1be23cdb874aea377077d7
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-11 19:16:41 +0500

    working

--- app/Http/Controllers/Inventory_DepartmentController.php
+++ app/Http/Controllers/Inventory_DepartmentController.php
@@ -90,9 +90,11 @@ class Inventory_DepartmentController extends Controller
 		}else{    
 		
         // return $invent_department->check_dept($request->get('deptname'),$request->get('code'));
+        if(!empty($request->get('code'))){
         if($invent_department->check_depart_code($request->get('code'))){
              return response()->json(array("state"=>1,"msg"=>'This department code already exists.',"contrl"=>'code'));
         }
+    }
 		if($invent_department->check_dept($request->get('deptname'))){
              return response()->json(array("state"=>1,"msg"=>'This department name already exists.',"contrl"=>'deptname'));
         }
