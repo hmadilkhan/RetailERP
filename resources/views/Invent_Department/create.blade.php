aec755b6def171122392d8f6338d016e1f1c54d7
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-11 19:07:54 +0500

    working

--- resources/views/Invent_Department/create.blade.php
+++ resources/views/Invent_Department/create.blade.php
@@ -252,6 +252,26 @@ $("#deptform").on('submit',function(event){
               });
     }
   });
+
+  function readURL(input,id) {
+		  if (input.files && input.files[0]) {
+			var reader = new FileReader();
+			
+			reader.onload = function(e) {
+			  $('#'+id).attr('src', e.target.result);
+			}
+			
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	$("#departImage").change(function() {
+	   readURL(this,'previewDepartImage');
+	});
+
+  $("#bannerImage").change(function() {
+	   readURL(this,'previewDepartBannerImage');
+	}); 
 </script>
 
 @endsection
