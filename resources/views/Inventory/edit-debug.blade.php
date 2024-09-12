111cf4bb1ebdb9a933b1d5e8cb2242e1f0b98852
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-12 16:08:25 +0500

    tag modal implement

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -458,6 +458,33 @@
 
             </section>
 
+            <div class="modal fade modal-flex" id="createtag-modal" tabindex="-1" role="dialog">
+                <div class="modal-dialog modal-md" role="document">
+                   <div class="modal-content">
+                      <div class="modal-header">
+                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                     <span aria-hidden="true">&times;</span>
+                                 </button>
+                         <h4 class="modal-title">Add Tag</h4>
+                      </div>
+                      <div class="modal-body">
+                        <div class="row">
+                              <div class="col-md-12">
+                               <div class="form-group"> 
+                                 <label class="form-control-label">Tag:</label>
+                                  <input type="text" name="tag_md" id="tag_md" class="form-control" />
+                                 </div>
+                               </div>
+                           </div>   
+                      </div>
+                      <div class="modal-footer">
+                         <button type="button" id="btn_tag_md" class="btn btn-success waves-effect waves-light" onClick="insertProduct_attribute('tag')">Add</button>
+                      </div>
+                   </div>
+                </div>
+             </div>                  
+
+
           <div class="modal fade modal-flex" id="uom-modal" tabindex="-1" role="dialog">
               <div class="modal-dialog modal-md" role="document">
                   <div class="modal-content">
