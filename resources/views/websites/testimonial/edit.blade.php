07a7909f8c13e36fc1dd184526358fdfc6f6b635
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-16 19:13:00 +0500

    working

--- resources/views/websites/testimonial/edit.blade.php
+++ resources/views/websites/testimonial/edit.blade.php
@@ -45,7 +45,7 @@
 
                 <div class="form-group">
                     <label class="control-label">Content <span class="text-danger">*</span></label>
-                    <textarea class="form-control" name="content" id="content" placeholder="Content" rows="5"></textarea>
+                    <textarea class="form-control" name="content" id="content" placeholder="Content" rows="5">{{ $testimonial->content }}</textarea>
                     @error('content')
                      <span class="form-control-feedback text-danger">{{ $message }}</span>
                     @enderror                
