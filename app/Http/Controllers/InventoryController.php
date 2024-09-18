733b751e70b68cce62fb12c5162174efb63c34fd
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-18 18:11:20 +0500

    working

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -1070,6 +1070,66 @@ class InventoryController extends Controller
             }
         }
 
+       //Product Gallery		
+       if (!empty($request->file('prodgallery'))) {
+        $count = 1;
+        foreach ($request->file('prodgallery') as $val) {
+            $prodGallery = $val;
+            $imageName   = null;
+            // $response    = Image::make($image)
+            //     ->save(public_path('storage/images/products/' . $imageName));
+
+            if (in_array(session('company_id'), [95, 102, 104]) || in_array(Auth::user()->username,['demoadmin','fnkhan'])) { //cloudinary image save fro kashee
+                $transformationArray = [];
+                $transformationArray['quality']  = 'auto';
+                $transformationArray['fetch']    = 'auto';
+
+                $company_name = DB::table('company')->where('company_id', session('company_id'))->first();
+
+                $folder = strtolower(str_replace(' ','',$company_name->name));
+
+                $imageName   = $productid.time().'-'.$count.'.'.$prodGallery->getClientOriginalExtension();
+
+                $imageData = Cloudinary::upload($prodGallery->getRealPath(), [
+                                        'public_id'      => strtolower($imageName),
+                                        'folder'         => $folder,
+                                        'transformation' => $transformationArray
+                                    ])->getSecurePath();
+                     
+            }else{
+                $path = public_path('storage/images/products/');
+                $returnImageValue = $this->uploads($prodGallery, $path);
+                $imageName = $returnImageValue['fileName']; 
+            }
+
+            if ($imageName != null) {
+                DB::table('inventory_images')->insert([
+                    "item_id" => $request->id,
+                    "image"   => $imageName,
+                    "url"     => isset($imageData) ? $imageData : null,
+                ]);
+
+                $count++;
+            }
+        }
+    }
+
+    //Product video
+    if (!empty($request->file('prodvideo'))) {
+        $prodVideo     = $request->file('prodvideo');
+        $path          = public_path('storage/video/products/');
+        $prodVideoName = $this->uploads($prodVideo, $path);
+
+        if (isset($prodVideoName['filename'])) {
+
+            DB::table('inventory_video')->insert([
+                "inventory_id" => $request->id,
+                "file"         => $$prodVideoName['filename'],
+                'created_at'   => date("Y-m-d H:i:s")
+            ]);
+        }
+    }       
+
 
         $this->sendPushNotification($request->code, $request->name, "update");
         //   return redirect()->back();
