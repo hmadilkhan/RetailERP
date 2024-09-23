2d4bb1960d805e968132204417dcb4ffb4aa38a8
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-23 20:23:09 +0500

    working

--- resources/views/order/online-order-details.blade.php
+++ resources/views/order/online-order-details.blade.php
@@ -90,6 +90,7 @@
                                     }
                                  $imageShow = !empty(Cloudinary::getUrl($getImage_id)) ? 'https://res.cloudinary.com/dl2e24m08/image/upload/f_webp,q_auto/'.$getImage_id.($extensionCount > 1 ? '.'.$getExtension : '') : asset('storage/images/placeholder.jpg') 
                               @endphp
+                              {{ $getExtension }}
                              @endif
                              <img src="{{ $imageShow }}" alt="" class="avatar-lg rounded productImage{{ $key }} " style="cursor:pointer;" onclick="showImage('{{ $key }}')">
                             @else
