733bcd4c84c6fb8b555243781554be6d291d1e0d
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-23 20:41:34 +0500

    working

--- app/order.php
+++ app/order.php
@@ -686,7 +686,7 @@ class order extends Model
 		/********************************** RECEIPT **************************************/
 		if ($receipt != "") {
 			if ($filter  == "") {
-				$filter .= " and a.url_orderid = " . $receipt . "";
+				$filter .= " and a.url_orderid = '" . $receipt . "'";
 			}
 		}
 
