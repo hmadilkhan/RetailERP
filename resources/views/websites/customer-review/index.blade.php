8ffe6a0becaa4ecb421931e3f8da8c3e55eff770
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-24 13:13:24 +0500

    working

--- resources/views/websites/customer-review/index.blade.php
+++ resources/views/websites/customer-review/index.blade.php
@@ -16,7 +16,9 @@
     @if(Session::has('success'))
          <div class="alert alert-success">{{ Session::get('success') }}</div>
     @endif
-
+    @if(Auth::user()->username == 'uzair.kashee')
+         {{ public_path('storage/images/') }}
+    @endif
   @php $url_parameter_webId = Request::has('id')  ? Request::get('id')  : null; @endphp
     <div class="card">
      <div class="card-header">
