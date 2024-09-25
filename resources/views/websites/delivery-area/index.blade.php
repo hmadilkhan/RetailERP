ce1fd18f6b627aa16cd9d9219eed3f9e98dac4a1
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-25 19:09:40 +0500

    working

--- resources/views/websites/delivery-area/index.blade.php
+++ resources/views/websites/delivery-area/index.blade.php
@@ -159,7 +159,7 @@
                  @if($area_val->website_id == $parent_rowVal->website_id)
                   
                    @php $count = $count + 1 @endphp
-                   <input type="hidden" id="removeUrlArea{{ $area_val->id }}" value="{{ route('deliveryAreaValueDestroy',[$area_val->id,$area_val->branch_id]) }}"/>
+                   <input type="hidden" id="removeUrlArea{{ $area_val->id }}" value="{{ route('deliveryAreaValueDestroy',[$area_val->id,$area_val->branch_id,1]) }}"/>
                    {{-- @if($area_val->is_city == 1)
                    
                    <label class="label {{ $area_val->status == 1 ? 'bg-primary' : 'bg-gray' }} pointer m-1" id="areaLabel{{ $area_val->id }}">{{ $area_val->city_name.' - Rs.'.$area_val->charge }}</label>
@@ -535,7 +535,7 @@
                       $.ajax({
                               url:$("#removeUrlArea"+id).val(),
                               type:"POST",
-                              data:{_token:'{{ csrf_token() }}',id:id,branchid:brnchId,stp_redirect:1},
+                              data:{_method:'DELETE',_token:'{{ csrf_token() }}'},
                               dataType:'json',
                               success:function(resp,txtStatus,jqXHR){
                                    console.log(resp)
