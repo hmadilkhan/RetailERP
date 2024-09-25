e675a45dec1229523e6f258be5ddae18a6013062
hmadilkhan <adilsziu@outlook.com>
2024-09-25 12:45:30 +0500

    Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4873,7 +4873,7 @@ class ReportController extends Controller
         $pdf->Cell(20, 7, 'Order No', 'B', 0, 'L', 1);
         $pdf->Cell(40, 7, 'Receipt No', 'B', 0, 'L', 1);
         $pdf->Cell(40, 7, 'Customer Name', 'B', 0, 'L', 1);
-        $pdf->Cell(30, 7, 'Amount', 'B', 0, 'L', 1);
+        $pdf->Cell(30, 7, 'Total Amount', 'B', 0, 'L', 1);
         $pdf->Cell(20, 7, 'Status', 'B', 0, 'L', 1);
         $pdf->Cell(20, 7, 'Date', 'B', 0, 'L', 1);
         $pdf->Cell(20, 7, 'Time', 'B', 1, 'C', 1);
@@ -4897,7 +4897,7 @@ class ReportController extends Controller
             $pdf->Cell(20, 6, $values->id, 0, 0, 'L', 1);
             $pdf->Cell(40, 6, $values->receipt_no, 0, 0, 'L', 1);
             $pdf->Cell(40, 6, $values->name, 0, 0, 'L', 1);
-            $pdf->Cell(40, 6, number_format($values->total_amount, 0), 0, 0, 'L', 1);
+            $pdf->Cell(30, 6, number_format($values->total_amount, 0), 0, 0, 'L', 1);
             $pdf->Cell(20, 6, $values->status, 0, 0, 'L', 1);
             $pdf->Cell(20, 6, date("d-m-Y",strtotime($values->date)), 0, 0, 'L', 1);
             $pdf->Cell(20, 6, date("h:i a",strtotime($values->time)), 0, 1, 'C', 1);
