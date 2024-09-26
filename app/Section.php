46c43dbd4cd678ac5b726895d435257fa8139f6a
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-26 20:52:17 +0500

    working

--- app/Section.php
+++ app/Section.php
@@ -13,4 +13,16 @@ class Section extends Model
     public static function getSection(){
         return DB::table('sections')->where('company_id',session('company_id'))->get();
     }
+
+    public static function customQuery_allColumFetch($condition,$mode,$count){
+        $filter = 'company_id = "'.session("company_id").'" and '.$condition;   
+         
+         if($mode == 1){
+             return DB::table('sections')->whereRaw($filter)->first();
+         }else{
+             $record = DB::table('sections')->whereRaw($filter)->get();
+             return ($count) ? count($record) : $record; 
+         }
+      }   
+
 }
