0a2efb5f476d7db96335d62a209f394a6211afe7
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-27 17:58:49 +0500

    working

--- resources/views/websites/delivery-area/index.blade.php
+++ resources/views/websites/delivery-area/index.blade.php
@@ -348,6 +348,111 @@
 
 @endsection
 
+
+@section('css_code')
+<style type="text/css">
+
+.container1 {
+  width: 480px; 
+  height: 240px; 
+  overflow-x: scroll;
+  overflow-y: hidden;
+}
+
+.container2 {
+  width: 480px; 
+  height: 330px; 
+  overflow-x: scroll;
+  overflow-y: hidden;
+}
+
+.inner {
+  height: 40px;
+  white-space:nowrap; 
+}
+
+.floatLeft {
+  width: 200px;
+  height: 180px; 
+  margin:10px 10px 50px 10px; 
+  display: inline-block;
+}
+
+.floatLeft1 {
+  width: 160px;
+  height: 200px; 
+  margin:10px 10px 50px 10px; 
+  display: inline-block;
+}
+
+/* Hide default HTML checkbox */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 43px;
+  height: 21px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+  /*content:'On';*/
+}
+
+input+.slider:before {
+	/*content: "Off";*/
+ }
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
+@endsection
+
 @section('scriptcode_three')
 
 
