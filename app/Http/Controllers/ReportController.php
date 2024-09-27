4b2ed8fcfc942f9d8d0effafa046232c190bb55d
hmadilkhan <adilsziu@outlook.com>
2024-09-27 11:13:51 +0500

    FBR Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4265,9 +4265,11 @@ class ReportController extends Controller
             $pdf->Cell(20, 7, number_format($totalsalestax, 2), 'B,T', 0, 'C');
             $pdf->Cell(35, 7, number_format($totalamount, 2), 'B,T', 1, 'C');
         }
+        $fileName = 'FBR_REPORT_' . date("M",strtotime($from))."_".$report->company_name . '.pdf';
         $pdfContent = $pdf->Output('F');
+        $filePath = 'pdfs/' . $fileName;
          // Set the file path where you want to save the PDF in the 'storage/app/public/pdfs' folder
-        Storage::disk('public')->put('app/public/pdfs/FBR_REPORT_' . date("M",strtotime($from))."_".$report->company_name . '.pdf',$pdfContent);
+        Storage::disk('public')->put($filePath,$pdfContent);
         //  $filePath = public_path('app/public/pdfs/FBR_REPORT_' . date("M",strtotime($from))."_".$report->company_name . '.pdf');
         // //save file
         // $pdf->Output($filePath, 'F');
