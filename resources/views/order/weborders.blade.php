fbcf2d9ae6135f30b56688772634f2aa609f034e
uzairrafiq <uzairrafiq_@hotmail.com>
2024-09-27 23:49:05 +0500

    working

--- resources/views/order/weborders.blade.php
+++ resources/views/order/weborders.blade.php
@@ -146,7 +146,7 @@
                                 <tr id="tbRow{{ $value->id }}" style="background-color:{{($value->isSeen == 1 ? '#efefef' : '')}}" >
                                     <td class="d-none">{{ $value->id }}</td>
                                     <td>{{$value->url_orderid}}</td>
-                                    <td>{{$value->date.' '.$value->time}}</td>
+                                    <td>{{$value->date}}</td>
                                     <td>{{ date("h:i a",strtotime($value->time)) }}</td>
                                     <td>{{$value->branch}}</td>
                                     <td>{{$value->name}}</td>
