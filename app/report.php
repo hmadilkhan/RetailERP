eb805536477b9d33fc78259795b11c5535f25f98
hmadilkhan <adilsziu@outlook.com>
2024-09-30 16:26:04 +0500

    Sales Person Report

--- app/report.php
+++ app/report.php
@@ -518,7 +518,7 @@ class report extends Model
         }else{
             $filter .= " and branch IN (select branch_id from branch where company_id = ".session('branch').")";
         }
-        $result = DB::select('SELECT d.id,d.fullname FROM sales_receipts a INNER JOIN sales_order_status b on b.order_status_id = a.status INNER JOIN customers c on c.id = a.customer_id INNER JOIN user_details d on d.id = a.sales_person_id WHERE a.date between ? and ?  ' . $filter.' group by d.fullname', [$fromdate, $todate,session('company_id')]);
+        $result = DB::select('SELECT d.id,d.fullname FROM sales_receipts a INNER JOIN sales_order_status b on b.order_status_id = a.status INNER JOIN customers c on c.id = a.customer_id INNER JOIN user_details d on d.id = a.sales_person_id WHERE a.date between ? and ?  ' . $filter.' group by d.fullname', [$fromdate, $todate]);
         return $result;
     }
 
