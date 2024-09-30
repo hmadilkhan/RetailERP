7b851741dda2556eb9eaa4e268e3f41fdf6a1e8b
hmadilkhan <adilsziu@outlook.com>
2024-09-30 16:25:27 +0500

    Sales person report query

--- app/report.php
+++ app/report.php
@@ -513,10 +513,12 @@ class report extends Model
         if ($salesperson != '' and $salesperson != "all") {
             $filter .= " and a.sales_person_id = " . $salesperson;
         }
-        // if ($branch != "" and $branch != "all") {
-        //     $filter .= " and a.branch = '" . $branch . "'";
-        // }
-        $result = DB::select('SELECT d.id,d.fullname FROM sales_receipts a INNER JOIN sales_order_status b on b.order_status_id = a.status INNER JOIN customers c on c.id = a.customer_id INNER JOIN user_details d on d.id = a.sales_person_id WHERE a.date between ? and ? and branch IN (select branch_id from branch where company_id = ?) ' . $filter.' group by d.fullname', [$fromdate, $todate,session('company_id')]);
+        if ($branch != "" and $branch != "all") {
+            $filter .= " and a.branch = '" . $branch . "'";
+        }else{
+            $filter .= " and branch IN (select branch_id from branch where company_id = ".session('branch').")";
+        }
+        $result = DB::select('SELECT d.id,d.fullname FROM sales_receipts a INNER JOIN sales_order_status b on b.order_status_id = a.status INNER JOIN customers c on c.id = a.customer_id INNER JOIN user_details d on d.id = a.sales_person_id WHERE a.date between ? and ?  ' . $filter.' group by d.fullname', [$fromdate, $todate,session('company_id')]);
         return $result;
     }
 
