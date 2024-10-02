8c02702b5730630598d3c8b063a513a1f9443aab
uzairrafiq <uzairrafiq_@hotmail.com>
2024-10-02 19:07:25 +0500

    working

--- resources/views/Inventory/variable-product-partial/variable-product-js-script.blade.php
+++ resources/views/Inventory/variable-product-partial/variable-product-js-script.blade.php
@@ -520,7 +520,7 @@
                                 "<option value='"+resp[count].id+"'>"+resp[count].name+"</option>");
                         }
                         
-                        if(status == 500){
+                        if(JxStat.status == 500){
                              swal({
                                     title: "Error!",
                                     text: resp,
