f23d532654e5b59ff3b8ee4bb16019aedf24ac1f
uzairrafiq <uzairrafiq_@hotmail.com>
2024-10-04 18:29:33 +0500

    working

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -84,10 +84,10 @@ class RouteServiceProvider extends ServiceProvider
     }
 
 
-    protected function configureRateLimiting()
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
-        });
-    }    
+    // protected function configureRateLimiting()
+    // {
+    //     RateLimiter::for('api', function (Request $request) {
+    //         return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+    //     });
+    // }    
 }
