1a04f965bc2aeb4cd0ea5ad8257e1dfff93e4082
uzairrafiq <uzairrafiq_@hotmail.com>
2024-10-07 17:19:35 +0500

    working

--- app/Http/Controllers/WebsiteImageController.php
+++ app/Http/Controllers/WebsiteImageController.php
@@ -147,8 +147,8 @@ class WebsiteImageController extends Controller
        // if you use a second parameter the package will not modify the original
         ImageOptimizer::optimize($imageUrl, '/home/u828600220/domains/sabsoft.com.pk/public_html/Retail/storage/images/optimize_images/1727867687-1726240292-ker-003.jpg');
         $headers = array(
-            'Content-Type'        => 'image/'.$extension,
-            'Content-Description' => $filename
+            'Content-Type'        => 'image/jpg',
+            'Content-Description' => '1727867687-1726240292-ker-003.jpg'
           ); 
         return response()->file('/home/u828600220/domains/sabsoft.com.pk/public_html/Retail/storage/images/optimize_images/1727867687-1726240292-ker-003.jpg', $headers);
     }
