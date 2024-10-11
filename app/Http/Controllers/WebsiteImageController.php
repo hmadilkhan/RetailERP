fd5381b17060bd2860dccdafb24112ba5cd17967
uzairrafiq <uzairrafiq_@hotmail.com>
2024-10-11 17:06:54 +0500

    working

--- app/Http/Controllers/WebsiteImageController.php
+++ app/Http/Controllers/WebsiteImageController.php
@@ -228,7 +228,7 @@ class WebsiteImageController extends Controller
 
               if(strtolower(pathinfo($imageName,PATHINFO_EXTENSION)) == 'png'){
                 // Now process the copied image
-                $process = new Process(['pngquant', '--quality=60-75', '--output', $optimizedPath]);
+                $process = new Process(['optipng', '-02', $optimizedPath]);
               }
 
             $process->run();
