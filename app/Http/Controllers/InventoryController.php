409241eaab402d8c9dfa28b379ae308df71db49f
uzairrafiq <uzairrafiq_@hotmail.com>
2024-10-25 17:37:37 +0500

    working

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2634,7 +2634,7 @@ class InventoryController extends Controller
                                                 ->where('product_id',$request->id)
                                                 ->where('branch_id', session('branch'))
                                                 ->where('status_id',1)
-                                                ->pluck('post_item_id')
+                                                ->pluck('pos_item_id')
                                 )
                     ->where('variation_id',$request->variationId)
                     ->pluck('product_id');  // get pos product id already exists this variation ($request->variationId)
