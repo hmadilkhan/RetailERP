dd520830e82b5fb7b32ab6db7947b84a7318d41d
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-01 20:04:57 +0500

    working

--- resources/views/Inventory/listnew.blade.php
+++ resources/views/Inventory/listnew.blade.php
@@ -240,6 +240,12 @@
 @section('css_code')
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+
+ <Style>
+    .dropdown-menu{
+        right:0;
+    }
+ </style>
 @endsection
 
 @section('scriptcode_three')
