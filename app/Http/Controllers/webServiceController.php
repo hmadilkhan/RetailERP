bbea2367c73cacbedcbaea463c05bc8c758e9a79
hmadilkhan <adilsziu@outlook.com>
2024-11-05 16:11:34 +0500

    Webservice

--- app/Http/Controllers/webServiceController.php
+++ app/Http/Controllers/webServiceController.php
@@ -18,6 +18,7 @@ class WebserviceController extends Controller
         $dash = new dashboard();
         
         $opening = SalesOpening::where("opening_id", $openingId)->where("status", 2)->first();
+        $date = $opening->date;
         $terminals = DB::select("SELECT d.company_id,d.name as company,d.logo,c.branch_id,c.branch_name as branch, b.terminal_name as terminal, a.permission_id,a.terminal_id FROM users_sales_permission a INNER JOIN terminal_details b on b.terminal_id = a.terminal_id INNER JOIN branch c on c.branch_id = b.branch_id INNER JOIN company d on d.company_id = c.company_id where a.Email_Reports = 1 and b.status_id = 1 and b.terminal_id = ?",[$opening->terminal_id]);
         foreach ($terminals as $key => $terminal) {
             $emails  = DB::table("branch_emails")->where("branch_id", $terminal->branch_id)->pluck("email");
@@ -51,6 +52,7 @@ class WebserviceController extends Controller
                     } // Details not found
                 } // Opening Id not found
             } // Email Not found bracket
+            //
         } 
         return 1;
     }
