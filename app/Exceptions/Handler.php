9f8d98b9c9f1613a1563076db0d429c3b1b82468
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-11 16:07:50 +0500

    working

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -50,11 +51,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-
-        if ($exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException && $exception->getStatusCode() == 419) {
+        // Check if the exception is a TokenMismatchException (which causes a 419 error)
+        if ($exception instanceof TokenMismatchException) {
+            // Redirect to the login page when CSRF token is invalid or expired
             return redirect()->route('login')->with('error', 'Session expired. Please login again.');
         }
-
 
         return parent::render($request, $exception);
     }
