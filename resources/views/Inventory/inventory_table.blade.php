b2e47e181e7296e11ab01aed8bac5959c6bae983
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-11 15:45:52 +0500

    working

--- resources/views/Inventory/inventory_table.blade.php
+++ resources/views/Inventory/inventory_table.blade.php
@@ -1,5 +1,5 @@
 <div class="project-table table-responsive">
-    <table id="inventtbl" class="table table-striped nowrap dt-responsive m-t-10 dataTable no-footer dtr-inline">
+    <table id="inventtbl" class="table table-striped nowrap m-t-10 dataTable">
         <thead>
             <tr>
                 <th style="text-align:center;vertical-align: middle;">
