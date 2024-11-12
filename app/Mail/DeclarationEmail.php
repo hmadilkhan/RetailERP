2f2924059eb7e243770ac0b644808d498956a417
hmadilkhan <adilsziu@outlook.com>
2024-11-12 16:32:47 +0500

    Report

--- app/Mail/DeclarationEmail.php
+++ app/Mail/DeclarationEmail.php
@@ -64,7 +64,7 @@ class DeclarationEmail extends Mailable
     public function attachments(): array
     {
         return [
-            // Attachment::fromPath(asset("/storage/declarationpdfs/" . 'sales_declaration_report_' . $this->declarationNumber  . '.pdf'))
+            Attachment::fromPath(storage_path("app/public/declarationpdfs/" . 'sales_declaration_report_' . $this->declarationNumber  . '.pdf'))
         ];
     }
 
