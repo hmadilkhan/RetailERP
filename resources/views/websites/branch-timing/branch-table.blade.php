22ed9211cedbe4b60a27ad02f8ef3a67f8c35ced
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-13 14:50:18 +0500

    working

--- resources/views/websites/branch-timing/branch-table.blade.php
+++ resources/views/websites/branch-timing/branch-table.blade.php
@@ -48,7 +48,6 @@
                 interval: 60,
                 minTime: '10',
                 maxTime: '6:00pm',
-                defaultTime: '11',
                 startTime: '10:00',
                 dynamic: false,
                 dropdown: true,
