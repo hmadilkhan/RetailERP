70ad7ae06ff0a0b03f503ab51f33de828b1de34b
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-13 14:53:53 +0500

    working

--- resources/views/websites/branch-timing/branch-table.blade.php
+++ resources/views/websites/branch-timing/branch-table.blade.php
@@ -44,7 +44,7 @@
         if ($(this).attr('type') === 'text') {
              console.log($(this).attr('id'));
             $(this).timepicker({
-                timeFormat: 'h:mm',
+                timeFormat: 'h:mm a',
                 interval: 60,
                 minTime: '10',
                 maxTime: '6:00pm',
