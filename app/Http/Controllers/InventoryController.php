32b37860dd418b091a1b1b2f502d223a2100f3dd
hmadilkhan <adilsziu@outlook.com>
2024-11-14 09:38:36 +0500

    Change validation of Product Name in inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -164,7 +164,7 @@ class InventoryController extends Controller
 
         $rules = [
             'code'          => 'required',
-            'name'          => 'required|regex:/^[A-Za-z0-9]+$/',
+            'name'          => 'required|regex:/^[a-zA-Z0-9\s\-]+$/',
             'reminder'      => 'required',
             'uom'           => 'required',
             'cuom'          => 'required',
