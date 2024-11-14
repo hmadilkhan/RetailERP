31b3025d76743c009cb68470da8139673f7a8699
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-14 19:10:32 +0500

    working

--- resources/views/Inventory/listnew.blade.php
+++ resources/views/Inventory/listnew.blade.php
@@ -7,7 +7,7 @@
 @section('navinventorys', 'active')
 
 @section('content')
-    <section class="panels-wells p-t-30">
+    <section class="panels-wells p-t-30 sm-m-t-50">
         <div class="card">
             <div class="card-header">
                 <h5 class="card-header-text">Upload Inventory</h5>
