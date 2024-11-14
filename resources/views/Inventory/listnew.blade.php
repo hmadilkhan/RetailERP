8a512888edb2300a5cf3ad545bed2427cd7faafc
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-14 17:48:07 +0500

    working

--- resources/views/Inventory/listnew.blade.php
+++ resources/views/Inventory/listnew.blade.php
@@ -241,13 +241,7 @@
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
   <style type="text/css">
-    .badge .tag-label{
-        padding: 0.2rem;
-        margin-right: 0.2rem;
-        font-size: 13px;
-        font-weight: 100;
-        border-radius: 0.2rem;
-    }
+
 /* Initially hide the child rows
 .child {
     display: none;
