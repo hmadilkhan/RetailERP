b845a387587f599d91fdeed3e4380cad3ae179c2
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-14 14:11:56 +0500

    working

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -725,7 +725,7 @@ $("#showProductWebsite").on('click',function(){
 		  let actionUrl = form.attr('action');
           let process = true;
         //   let regex = /[^a-zA-Z0-9\s]/g;
-          let regex = /[^a-zA-Z0-9\s\u0600-\u06FF\u0750-\u077F]/g;
+          let regex = /[^a-zA-Z0-9\s\u0600-\u06FF\u0750-\u077F()]/g;
           if(regex.test($("#name").val())){
             process = false;
             swal('Error!','Special characters are not allowed!','error');
