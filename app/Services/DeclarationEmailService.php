b6eb38ee239d9ce283740403635073aaf6ae970a
hmadilkhan <adilsziu@outlook.com>
2024-11-20 13:09:49 +0500

    Declartation Service

--- app/Services/DeclarationEmailService.php
+++ app/Services/DeclarationEmailService.php
@@ -494,8 +494,9 @@ class DeclarationEmailService
     {
         $users = new userDetails();
         $dash = new dashboard();
-        $date = date("Y-m-d", strtotime("-1 day"));
-        $terminals = DB::select("SELECT d.company_id,d.name as company,d.logo,c.branch_id,c.branch_name as branch, b.terminal_name as terminal, a.permission_id,a.terminal_id FROM users_sales_permission a INNER JOIN terminal_details b on b.terminal_id = a.terminal_id INNER JOIN branch c on c.branch_id = b.branch_id INNER JOIN company d on d.company_id = c.company_id where a.Email_Reports = 1 and b.status_id = 1");
+        $opening = SalesOpening::where("opening_id", $openingId)->where("status", 2)->first();
+        $date = $opening->date;
+        $terminals = DB::select("SELECT d.company_id,d.name as company,d.logo,c.branch_id,c.branch_name as branch, b.terminal_name as terminal, a.permission_id,a.terminal_id FROM users_sales_permission a INNER JOIN terminal_details b on b.terminal_id = a.terminal_id INNER JOIN branch c on c.branch_id = b.branch_id INNER JOIN company d on d.company_id = c.company_id where a.Email_Reports = 1 and b.status_id = 1 and b.terminal_id = ?", [$opening->terminal_id]);
         foreach ($terminals as $key => $terminal) {
             $emails  = DB::table("branch_emails")->where("branch_id", $terminal->branch_id)->pluck("email");
             if (!empty($emails)) {
@@ -505,9 +506,12 @@ class DeclarationEmailService
                 $settings = DB::table("settings")->where("company_id", $terminal->company_id)->first();
                 $settings = !empty($settings) ? json_decode($settings->data) : '';
                 $currency = !empty($settings) ? $settings->currency : 'Rs.';
-                $opening = SalesOpening::where("terminal_id", $terminal->terminal_id)->where("date", $date)->where("status", 2)->first();
                 $companyLogo = "https://retail.sabsoft.com.pk/storage/images/company/" . $terminal->logo;
                 if (!empty($opening)) {
+                    // if (count($emails) == 1) {
+                    //     $emails = $emails[0];
+                    // }
+                    // return $emails;
                     $permissions = $users->getPermission($terminal->terminal_id);
                     $terminal_name = $users->getTerminalName($terminal->terminal_id);
                     $heads = $dash->getheadsDetailsFromOpeningIdForClosing($opening->opening_id);
@@ -521,12 +525,16 @@ class DeclarationEmailService
                         $subject = "Sales Declaration Email of " . $terminal_name[0]->branch_name . " (" . $terminal_name[0]->terminal_name . ") ";
                         $declarationNo =  $heads[0]->opening_id;
 
-                        $this->generatePdfReport($terminal->company_id, $terminal->branch_id, $terminal->terminal_id, $openingId);
-                        $emails = ["hmadilkhan@gmail.com"]; // ->cc(["hmadilkhan@gmail.com", "syedrazaali10@gmail.com", "humayunshamimbarry@gmail.com"])
-                        Mail::to($emails)->send(new DeclarationEmail($branchName, $subject, $declarationNo, $data, $currency, $date, $companyLogo));
+                        $this->generatePdfReport($terminal->company_id, $terminal->branch_id, $terminal->terminal_id, $opening->opening_id);
+                        // print($emails);
+                        //$emails = ["hmadilkhan@gmail.com"]; 
+
+                        Mail::to($emails)->cc(["humayunshamimbarry@gmail.com"])->send(new DeclarationEmail($branchName, $subject, $declarationNo, $data, $currency, $date, $companyLogo));
                     } // Details not found
                 } // Opening Id not found
             } // Email Not found bracket
-        } 
+            //
+        }
+        return 1;
     }
 }
