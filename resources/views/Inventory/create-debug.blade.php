a2aa2459fc7c8a47c4f85d3c61d0554fc7b60c4f
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-25 17:54:24 +0500

    working

--- resources/views/Inventory/create-debug.blade.php
+++ resources/views/Inventory/create-debug.blade.php
@@ -519,7 +519,7 @@
                   <div class="card-block p-2 p-t-0">
               <div class="form-group">
                        <a href="javascript:void(0)">
-                        <img id="simg" src="{{ asset('storage/images/placeholder.jpg') }}" class="thumb-img width-100" height="380px" alt="img">
+                        <img id="simg" src="{{ asset('storage/images/placeholder.jpg') }}" class="max-height-100 width-100" alt="img">
                         </a>
 
                     <div class="form-group {{ $errors->has('image') ? 'has-danger' : '' }} m-t-10">
