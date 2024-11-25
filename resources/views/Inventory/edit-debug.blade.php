dffe53544a673a79a3ce1ffaf68e8f8d535c0403
uzairrafiq <uzairrafiq_@hotmail.com>
2024-11-25 17:57:58 +0500

    working

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -396,7 +396,7 @@
                                @endif --}}
                         {{-- @endif --}}
                             <a href="{{ $imageUrl }}" data-toggle="lightbox" data-title="{{$data[0]->product_name}}">
-                                <img id="simg" src="{{ $imageUrl }}" class="thumb-img width-100" height="380px" alt="{{ empty($data[0]->image) ? $data[0]->image : 'placeholder.jpg' }}">
+                                <img id="simg" src="{{ $imageUrl }}" class="max-height-100 width-100" alt="{{ empty($data[0]->image) ? $data[0]->image : 'placeholder.jpg' }}">
                                 </a>
                                 <br/>
                                 <label for="image" class="custom-file">
