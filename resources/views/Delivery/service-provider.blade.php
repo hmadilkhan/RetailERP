96092d3ed40ee7d55edee4e98bca1f64caa09546
hmadilkhan <adilsziu@outlook.com>
2024-12-06 14:09:07 +0500

    Store

--- resources/views/Delivery/service-provider.blade.php
+++ resources/views/Delivery/service-provider.blade.php
@@ -228,7 +228,7 @@
    <div class="col-md-4">
      <label for="image" class="form-control-label">Image</label>
      <a href="#">
-       <img id="simg" src="{{ asset('assets/images/placeholder.jpg') }}" class="thumb-img img-fluid width-100" alt="img" style="width: 100px;height: 100px;">
+       <img id="simg" src="{{ asset('storage/images/placeholder.jpg') }}" class="thumb-img img-fluid width-100" alt="img" style="width: 100px;height: 100px;">
      </a>
      <div class="form-group {{ $errors->has('image') ? 'has-danger' : '' }} m-t-10">
        <label for="image" class="custom-file">
