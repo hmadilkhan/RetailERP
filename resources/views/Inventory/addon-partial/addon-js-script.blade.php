521afa82dd4ae2a1e8767208b20bcdb12016e371
uzairrafiq <uzairrafiq_@hotmail.com>
2024-12-20 20:20:28 +0500

    working

--- resources/views/Inventory/addon-partial/addon-js-script.blade.php
+++ resources/views/Inventory/addon-partial/addon-js-script.blade.php
@@ -566,7 +566,6 @@ $("#subDepartment_cpymd").on('change',function(){
 
 
 function get_allGeneralItemWithAddonBind(depart_val,subDepart_val,addonHeadId,addonHeadName){
-    console.log(depart_val+'-'+subDepart_val+'-'+addonHeadId+'-'+addonHeadName)
               $.ajax({
                 url: "{{ route('get_generalItem_withoutAddonBind') }}",
                 type: 'POST',
