44d39ad2a67729ec7c78b18a987a41a783b61b45
hmadilkhan <adilsziu@outlook.com>
2024-12-31 14:55:34 +0500

    Expense

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -179,7 +179,7 @@ class ExpenseController extends Controller
         $pdf->AliasNbPages();
         $pdf->AddPage();
 
-        if (!file_exists(asset('assets/images/company/qrcode.png')))
+        if (!file_exists(asset('storage/images/company/qrcode.png'))) {
         {
             $qrcodetext = $company[0]->name." | ".$company[0]->ptcl_contact." | ".$company[0]->address;
             \QrCode::size(200)
@@ -196,10 +196,10 @@ class ExpenseController extends Controller
         //second row
         $pdf->SetFont('Arial','B',14);
         $pdf->Cell(35,0,'',0,0);
-        $pdf->Image(asset('assets/images/company/'.$company[0]->logo),12,10,-200);
+        $pdf->Image(asset('storage/images/company/' . $company[0]->logo), 12, 10, -200);
         $pdf->Cell(105,12,$company[0]->name,0,0,'L');
         $pdf->Cell(50,0,"",0,1,'R');
-        $pdf->Image(asset('assets/images/company/qrcode.png'),175,10,-200);
+        $pdf->Image(asset('storage/images/company/qrcode.png'), 175, 10, -200);
 
         //third row
         $pdf->SetFont('Arial','',10);
