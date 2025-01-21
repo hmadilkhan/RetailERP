26dd365558476377494b50f8d0e6ed5c4823c7ed
uzairrafiq <uzairrafiq_@hotmail.com>
2025-01-21 18:53:12 +0500

    working

--- resources/views/websites/delivery-area/index.blade.php
+++ resources/views/websites/delivery-area/index.blade.php
@@ -346,7 +346,7 @@
 
 
       <div class="modal fade modal-flex" id="DeliveryparentDetail_Modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-dialog modal-sm" role="document">
            <div class="modal-content">
               <div class="modal-header">
                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
