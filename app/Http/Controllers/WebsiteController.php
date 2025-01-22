a44d4fdfcd8c9f4684cd29ead57af9c2d5c878f1
uzairrafiq <uzairrafiq_@hotmail.com>
2025-01-22 20:29:27 +0500

    working

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -564,7 +564,7 @@ class WebsiteController extends Controller
         $Slide        = $request->file('desktop_slide');
         $mobile_slide = $request->file('mobile_slide');
         $products     = $request->product_dpt_slide;
-           return $request;
+        //    return $request;
 
         $columnArray = ['updated_at' => date("Y-m-d H:i:s")];
 
