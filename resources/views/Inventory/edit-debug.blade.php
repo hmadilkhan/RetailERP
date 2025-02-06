6abf574a0e87c770c27acf64a161277e6fa6fff9
uzairrafiq <uzairrafiq_@hotmail.com>
2025-02-06 13:22:08 +0500

    working

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -675,11 +675,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/md5-js-tools@1.0.2/lib/md5.min.js"></script>
 	<script type="text/javascript">
 
-       @if(old('description'))
-          $("#description").val("{{ old('description') }}")
-       @else
-          $("#description").val('{{ $product_description }}');
-       @endif
+
 
        $(".select2").select2();
 
