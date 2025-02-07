ae156194ccf132fe56d2b119af4be033254597b1
uzairrafiq <uzairrafiq_@hotmail.com>
2025-02-07 18:18:12 +0500

    working website short description and details

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -286,18 +286,32 @@
 
         <div class="row">
             <div class="col-lg-6 col-md-6">
+                @php $short_description = ''; @endphp
+                @if(!empty($data[0]->short_description))
+                    @php
+                        $short_description = htmlentities($data[0]->short_description);
+                        $short_description = html_entity_decode($short_description);
+                    @endphp
+                @endif
                 <div class="form-group ">
                     <label class="form-control-label">Short Description <i>(For Website Only) - Features</i></label>
-                    <textarea class="form-control" name="sdescription" id="summary-ckeditor" rows="3" >{{ old('sdescription') }}</textarea>
+                    <textarea class="form-control" name="sdescription" id="summary-ckeditor" rows="3" >{{ $short_description }}</textarea>
                     @if($errors->has('sdescription'))
                         <div class="form-control-feedback">Required field can not be blank.</div>
                     @endif
                 </div>
             </div>
             <div class="col-lg-6 col-md-6">
+                @php $product_details = ''; @endphp
+                @if(!empty($data[0]->details))
+                    @php
+                        $product_details = htmlentities($data[0]->details);
+                        $product_details = html_entity_decode($product_details);
+                    @endphp
+                @endif                
                 <div class="form-group ">
                     <label class="form-control-label">Details <i>(For Website Only) - Applications</i></label>
-                    <textarea class="form-control" name="details" id="details" rows="6"></textarea>
+                    <textarea class="form-control" name="details" id="details" rows="6">{{ $product_details }}</textarea>
                     @if($errors->has('details'))
                         <div class="form-control-feedback">Required field can not be blank.</div>
                     @endif
