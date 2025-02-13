6012367aea0c44890f2c5c62e7e3f50b8c46744f
uzairrafiq <uzairrafiq_@hotmail.com>
2025-02-13 18:15:40 +0500

    working on validation brand logo and banner image validation text color add text-danger

--- resources/views/Inventory/brands/index.blade.php
+++ resources/views/Inventory/brands/index.blade.php
@@ -25,13 +25,13 @@
               <div class="card-header">
                   <h5 class="card-header-text">{{ isset($id) ? 'Edit' : 'Create' }} Brand</h5>
                     @if(isset($id))
-                      
+
                          <a href="{{ route('brands.index') }}" class="f-right">Back to lists</a>
-                    @endif                 
+                    @endif
               </div>
               <div class="card-block">
-                  
-                 @php 
+
+                 @php
                    $route = route("brands.store");
                     if(isset($id)){
                          $route = route("brands.update",$id);
@@ -39,121 +39,121 @@
                  @endphp
                  <form method="post" class="form-horizontal" enctype="multipart/form-data" action="{{ $route }}">
                      @csrf
-                     
+
                      @if(isset($id))
                         @method('PATCH')
                      @endif
-                     
+
                             <div class="form-group">
-                                 @php 
+                                 @php
                                     $brandName = old('name');
-                                   
+
                                     if(isset($id)){
                                        $brandName = old('name') ? old('name') : $edit->name;
                                     }
-                                 @endphp    
+                                 @endphp
                                 <label>Brand Name</label>
                                 <input type="text" class="form-control @error('name') 'has-danger' @enderror" placeholder="Brand Name like 'nikke'" name="name" id="name"
                                 value="{{ $brandName }}">
-                              @error('name')    
+                              @error('name')
                                 <span class="text-danger" id="name_alert">{{ $message }}</span>
-                              @enderror    
+                              @enderror
                             </div>
-    
+
                             <div class="form-group">
-                                 @php 
+                                 @php
                                     $brandSlug = old('slug');
-                                   
+
                                     if(isset($id)){
                                        $brandSlug = old('slug') ? old('slug') : $edit->slug;
                                     }
-                                 @endphp                                 
+                                 @endphp
                                 <label>Slug</label>
                                 <input type="text" class="form-control" placeholder="Slug" name="slug" id="slug" value="{{ $brandSlug }}">
                                 <span class="text-danger" id="slug_alert"></span>
                             </div>
-                            
+
                             <div class="form-group">
                                 <label>Parent</label>
                                 <select class="form-control select2" placeholder="Parent" name="parent" id="parent">
                                     <option value="">Select</option>
                                     @if($lists)
-                                       @php 
+                                       @php
                                            $parent = old('parent');
                                              if(isset($id)){
                                                 $parent = $edit->parent;
                                              }
-                                       @endphp     
+                                       @endphp
                                       @foreach($lists as $val)
                                         @if($val->parent == null)
                                          <option {{ $parent == $val->id ? 'selected' : '' }} value="{{ $val->id }}">{{ $val->name }}</option>
                                         @endif
                                       @endforeach
                                     @endif
-                                </select>    
+                                </select>
                                 <span class="text-danger" id="parent_alert"></span>
-                            </div>                            
-    
+                            </div>
+
                             <div class="form-group">
-                                @php 
+                                @php
                                    $priority = old('priority') ? old('priority') : 0;
                                      if(isset($id)){
                                         $priority = $edit->priority ? $edit->priority : 0;
                                      }
-                                @endphp                                 
+                                @endphp
                                 <label>Priority</label>
                                 <input type="number" min="0" value="{{ $priority }}" class="form-control" placeholder="Priority" name="priority" id="priority">
                                 <span class="text-danger" id="priority_alert"></span>
                             </div>
-                            
+
                             <div class="form-group">
-                                 @php 
+                                 @php
                                     $brandImage = asset('storage/images/placeholder.jpg');
-                                   
+
                                     if(isset($id)){
                                        $brandImage = asset('storage/images/brands/'.session('company_id').'/'.$edit->image);
                                     }
-                                 @endphp                                  
-                               <label class="form-control-label">Image</label>
+                                 @endphp
+                               <label class="form-control-label">Brand Logo</label>
                                  <img id="showImage" src="{{ $brandImage }}" class="thumb-img img-fluid" alt="{{ isset($id) ?  $edit->image : 'placeholder.jpg' }}" width="100px" height="100px">
-                               
+
                                   <label for="image" class="custom-file">
                                        <input type="file" id="image" name="image" class="custom-file-input">
                                        <span class="custom-file-control"></span>
                                   </label>
                                 @if ($errors->has('image'))
-                                    <div class="form-control-feedback">{{ $errors->first('image') }}</div>
+                                    <span class="text-danger">{{ $errors->first('image') }}</span>
                                 @endif
-                            </div> 
-                            
-                            
+                            </div>
+
+
                             <div class="form-group">
-                                 @php 
+                                 @php
                                     $brandBanner = asset('storage/images/placeholder.jpg');
-                                   
+
                                     if(isset($id)){
                                        $brandBanner = asset('storage/images/brands/'.session('company_id').'/'.$edit->banner);
                                     }
-                                 @endphp                                  
+                                 @endphp
                                <label class="form-control-label">Banner</label>
                                  <img id="showBanner" src="{{ $brandBanner }}" class="thumb-img" alt="{{ isset($id) ?  $edit->banner : 'placeholder.jpg' }}" width="380px" height="128px">
-                               
+
                                   <label for="banner" class="custom-file">
                                        <input type="file" id="banner" name="banner" class="custom-file-input">
                                        <span class="custom-file-control"></span>
                                   </label>
                                 @if ($errors->has('banner'))
-                                    <div class="form-control-feedback">{{ $errors->first('banner') }}</div>
+                                    <span class="text-danger">{{ $errors->first('banner') }}</span>
                                 @endif
-                            </div>                             
-                            
+                            </div>
+
     		            <button type="submit" class="btn btn-success f-right">{{ isset($id) ? 'Save Changes' : 'Submit' }}</button>
-    		       </form> 
+    		       </form>
              </div>
           </div>
         </div>
-        
-        
+
+
         <div class="col-md-6">
         <div class="card">
             <div class="card-header">
@@ -174,32 +174,32 @@
                     </thead>
                     <tbody>
                       @foreach($lists as $brand)
-                        @php 
+                        @php
                             $parentTable = $lists->where('id',$brand->parent)->pluck('name');
-                        @endphp    
+                        @endphp
                          <tr id="row-"{{ $brand->id }}>
                              <td class="d-none">{{ $brand->priority }}</td>
                              <td>
-                                 @php 
-                                   $image = asset('storage/images/placeholder.jpg');   
+                                 @php
+                                   $image = asset('storage/images/placeholder.jpg');
                                     if($brand->image != null){
                                        $path = 'storage/images/brands/'.session('company_id').'/'.$brand->image;
                                        $image = File::exists($path) ? asset('storage/images/brands/'.session('company_id').'/'.$brand->image) : asset('storage/images/placeholder.jpg');
                                     }
-                                 @endphp    
+                                 @endphp
                                  <img src="{{ $image }}" class="thumb-img img-fluid" alt="{{ $brand->image == '' ? 'placeholder.jpg' : $brand->image }}" width="100px" height="100px">
-                                 
+
                                  @if($brand->banner != null)
                                   @php
                                    $path = 'storage/images/brands/'.session('company_id').'/'.$brand->banner;
                                      if(File::exists($path)){
-                                  @endphp  
+                                  @endphp
                                     <br/>
                                          <img src="{{ asset('storage/images/brands/'.session('company_id').'/'.$brand->banner) }}" class="thumb-img img-fluid" alt="{{ $brand->banner }}" width="100px" height="100px">
-                                  @php         
+                                  @php
                                      }
-                                  @endphp 
-                                 
+                                  @endphp
+
                                  @endif
                                </td>
                              <td>{{ $brand->name }}</td>
@@ -210,30 +210,30 @@
                                 <a href="{{ route('brands.edit',$brand->id) }}" class="m-r-10" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit" ><i class="icofont icofont-ui-edit text-primary f-18"></i> </a>
 
                                 <a data-toggle="tooltip" data-placement="top" title="" data-original-title="Delete" onclick="remove({{ $brand->id }},'{{ $brand->name }}')"><i class="icofont icofont-ui-delete text-danger f-18"></i></a>
-                                
+
                                 <form action="{{ route('brands.destroy',$brand->id) }}" class="d-none" id="removeForm{{ $brand->id }}" method="post">
                                     @csrf
                                     @METHOD('DELETE')
                                 </form>
                              </td>
                          </tr>
-                      @endforeach    
+                      @endforeach
                     </tbody>
                 </table>
             </div>
-        </div>            
-            
-            
         </div>
-        
-        
+
+
+        </div>
+
+
     </div>
-	
- </section> 
- 
+
+ </section>
 
 
-              
+
+
 @endsection
 
 @section('scriptcode_one')
@@ -242,12 +242,12 @@
 
 @section('scriptcode_three')
 
-<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script> 
+<script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
 
 <script type="text/javascript">
 
    $(".select2").select2();
-   
+
    $('#table_brand').DataTable({
         bLengthChange: true,
         displayLength: 10,
@@ -264,29 +264,29 @@
 
     function readURL(input,elementId) {
         if (input.files && input.files[0]) {
-            
+
             var reader = new FileReader();
             reader.onload = function(e) {
               $('#'+elementId).attr('src', e.target.result);
-              
+
               $(".custom-file-control:lang(en)::after").text(e.target.result)
             }
             reader.readAsDataURL(input.files[0]);
         }
     }
-    
-    
+
+
     $("#banner").on('change',function(){
         readURL(this,'showBanner');
-    })    
-        
-    
+    })
+
+
     $("#image").on('change',function(){
         readURL(this,'showImage');
     })
-    
+
     function remove(id,name){
-  
+
             swal({
                 title: "DELETE BRAND",
                 text: "Do you want to delete brand "+name+"?",
@@ -299,13 +299,13 @@
                 closeOnCancel: false
             },function(isConfirm){
                 if(isConfirm){
-                      $("#removeForm"+id).submit();  
+                      $("#removeForm"+id).submit();
                 }else{
                     swal.close();
                 }
-            });		            
-        
-        
+            });
+
+
     }
 
 </script>
