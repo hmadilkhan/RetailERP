97cc647abeca85a7529f0d1c879c54147e2004d5
uzairrafiq <uzairrafiq_@hotmail.com>
2025-02-20 19:28:17 +0500

    working color setting

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,12 +46,12 @@
         }
 
         #projectedSales div:hover {
-            background: #198754;
+            background: #4CAF50;
             color: #ffffff;
         }
-
+        /*#198754*/
         #closedSales div:hover {
-            background: #198754;
+            background: #4CAF50;
             color: #ffffff;
         }
     </style>
