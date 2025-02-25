3134e75b27e9b8225ddbc478c1b63eec7f90ce60
uzairrafiq <uzairrafiq_@hotmail.com>
2025-02-25 17:37:08 +0500

    working image resizer

--- app/Traits/MediaTrait.php
+++ app/Traits/MediaTrait.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 // use Image;
 use Intervention\Image\Laravel\Facades\Image;
+use Intervention\Image\Facades\Image;
 // use Carbon\gd;
 use Intervention\Image\ImageManager;
 trait MediaTrait
