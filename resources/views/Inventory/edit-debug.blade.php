99b0ce99eca2d1ebdd70bbe6333a373de84f8ae2
uzairrafiq <uzairrafiq_@hotmail.com>
2025-03-17 12:56:04 +0500

    working

--- resources/views/Inventory/edit-debug.blade.php
+++ resources/views/Inventory/edit-debug.blade.php
@@ -284,7 +284,7 @@
 
         </div>
 
-        <div class="row {{ $websiteType != 'restaurant' ? '' : 'hidden'}}" id="editorDiv">
+        <div class="row {{ $websiteType != 'restaurant' ? '' : 'd-none'}}" id="editorDiv">
             <div class="col-lg-6 col-md-6">
                 <div class="form-group">
 
@@ -321,7 +321,7 @@
             </div>
         </div>
           <div class="row">
-            <div class="col-md-12 {{ $websiteType == 'restaurant' ? 'hidden' : ''}}" id="nonEditorDiv">
+            <div class="col-md-12 {{ $websiteType == 'restaurant' ? 'd-none' : ''}}" id="nonEditorDiv">
                 <div class="form-group ">
                     <label class="form-control-label">Details <i>(For Website Only)</i></label>
                     <textarea class="form-control" name="product_description_resturant_website" id="product_description_resturant_website" rows="6">{{ $data[0]->product_description_resturant_website }}</textarea>
