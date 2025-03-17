3d5d6de7bb72bbe9c56b98f993826162a7d7e038
uzairrafiq <uzairrafiq_@hotmail.com>
2025-03-17 12:43:33 +0500

    working

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -1195,7 +1195,7 @@ class InventoryController extends Controller
 
         $websiteType = null;
         if($selectedWebsites != null && $websites != null){
-            $websiteType = $websites->whereIn('id',$selectedWebsites[0])->get();
+            $websiteType = $websites->where('id',$selectedWebsites[0])->get();
             $websiteType = $websiteType->type;
         }
 
