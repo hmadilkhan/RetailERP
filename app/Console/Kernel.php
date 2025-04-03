1194dc5d68a411429dabeef1f797bc9d3a581573
hmadilkhan <adilsziu@outlook.com>
2025-04-03 16:58:55 +0500

    RefreshToken

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\RefreshQuickBooksTokenJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -27,6 +28,7 @@ class Kernel extends ConsoleKernel
     {
 		// $schedule->command('test:run')->everyMinute();
 		$schedule->command('app:date-wise-stock-command')->dailyAt("12:00");
+        $schedule->job(new RefreshQuickBooksTokenJob())->everyThirtyMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
