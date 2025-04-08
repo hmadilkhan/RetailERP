6dddea1fc59353477e1d06b8f1d32dfacc5ac351
hmadilkhan <adilsziu@outlook.com>
2025-04-08 15:10:33 +0500

    Cron Commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,8 +29,9 @@ class Kernel extends ConsoleKernel
 		// $schedule->command('test:run')->everyMinute();
 		$schedule->command('app:date-wise-stock-command')->dailyAt("12:00");
         $schedule->job(new RefreshQuickBooksTokenJob())->everyThirtyMinutes();
-        $schedule->job(new \App\Jobs\SyncQuickBooksCustomersJob)->everyMinute();
-        $schedule->job(new \App\Jobs\SyncQuickBooksItemsJob)->everyMinute();
+        // $schedule->job(new \App\Jobs\SyncQuickBooksCustomersJob)->everyMinute();
+        // $schedule->job(new \App\Jobs\SyncQuickBooksItemsJob)->everyMinute();
+        $schedule->job(new \App\Jobs\SyncQuickBooksInvoiceJob)->everyMinute();
 
     }
 
