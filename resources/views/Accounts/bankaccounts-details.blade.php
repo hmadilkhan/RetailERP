351c501bcdca26a7dffd08ba29d7b113733a4fd3
uzairrafiq <uzairrafiq_@hotmail.com>
2025-04-11 18:41:13 +0500

    working on ui

--- resources/views/Accounts/bankaccounts-details.blade.php
+++ resources/views/Accounts/bankaccounts-details.blade.php
@@ -9,7 +9,7 @@
 
 @section('content')
 
-<section class="panels-wells p-3">
+<section class="panels-wells p-t-2">
 <div class="card">
      <div class="card-header">
          <h5 class="card-header-text">Create Bank Account</h5>
