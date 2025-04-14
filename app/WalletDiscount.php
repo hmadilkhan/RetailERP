b9c3a9a03b7fbd521c79096c90a7b480bafd7831
uzairrafiq <uzairrafiq_@hotmail.com>
2025-04-14 19:29:45 +0500

    working

--- app/WalletDiscount.php
+++ app/WalletDiscount.php
@@ -11,7 +11,7 @@ class WalletDiscount extends Model
     public function getWalletDiscount()
     {
         return DB::table('wallet_discount')
-                ->join('service_provider_details','service_provider_details.id','wallet_discount.wallet-id')
+                ->join('service_provider_details','service_provider_details.id','wallet_discount.wallet_id')
                 ->where('service_provider_details.categor_id',6)
                 ->where('service_provider_details.branch_id',session('branch'))
                 ->select('wallet_discount.*','service_provider_details.provider_name','service_provider_details.image')
