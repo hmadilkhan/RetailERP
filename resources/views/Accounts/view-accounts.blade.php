9e2de9ae4921a12ab6e93d258e2f0740925f8773
uzairrafiq <uzairrafiq_@hotmail.com>
2025-04-14 15:29:53 +0500

    working

--- resources/views/Accounts/view-accounts.blade.php
+++ resources/views/Accounts/view-accounts.blade.php
@@ -51,7 +51,7 @@
                        <input type="hidden" id="bankAccountId" value="{{ Crypt::encrypt($value->bank_account_id) }}"/>
                        <input type="hidden" id="websiteId" value="{{ Crypt::encrypt($value->website_id) }}"/>
                        <input type="hidden" id="websiteBankUniqueId" value="{{ Crypt::encrypt($value->website_bank_id) }}"/>
-                       <input type="hidden" id="actionWebsiteUrl" value="{{ isset($value->website_id) ? route('bankUnlinkToWebsite') : route('bankLinkToWebsite') }}"/>
+                       <input type="hidden" id="actionWebsiteUrl" value="{{ isset($value->website_id) ? route('bankUnLinkToWebsite') : route('bankLinkToWebsite') }}"/>
                       <a href="javascript:voide(0)" class="m-r-10" data-toggle="tooltip"
                          data-placement="top" title="" data-original-title="{{ isset($value->website_id) ?  'Unlink to website' : 'Link to website' }}"
                          onclick="bind_website({{ isset($value->website_id) ? 1 : 0 }})">
