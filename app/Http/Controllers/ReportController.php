05a6822dd474c3f04a080dae19b0c60c4f7874d4
hmadilkhan <adilsziu@outlook.com>
2025-04-24 14:41:17 +0500

    Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -423,7 +423,7 @@ class ReportController extends Controller
          ');
 
         // Get the view content
-        $html = view('reports.item-sale-report-export', [
+        $html = view('partial.reports.item-sale-report-export', [
             'record' => $record,
             'branch' => $branch,
             'dates' => $dates,
