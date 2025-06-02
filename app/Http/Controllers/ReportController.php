785582ed22f4e40389cb09f83a320ee75fda7333
hmadilkhan <adilsziu@outlook.com>
2025-06-02 16:32:07 +0500

    Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1483,36 +1483,54 @@ class ReportController extends Controller
             $pdf->Cell(40, 6, number_format($value->expenseamt, 2), 0, 1, 'R');
         }
 
+        // $expesnsetotal = ($expesnsetotal  ); //$purchases[0]->purchase_amount
+
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(95, 5, 'Total Expenses', 0, 0, 'L');
+        $pdf->Cell(95, 5, number_format($expesnsetotal, 2), 0, 1, 'R');
+
         $pdf->SetFont('Arial', 'B', 12);
         $pdf->setFillColor(230, 230, 230);
         $pdf->SetTextColor(0, 0, 0);
         $pdf->Cell(95, 8, "Discounts", 0, 0, 'L', 1); //your cell
         $pdf->Cell(95, 8, "Rs. " . number_format($discounts[0]->discounts, 2), 0, 1, 'R', 1); 
+        
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->setFillColor(230, 230, 230);
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->Cell(95, 8, "Sales Return", 0, 0, 'L', 1); //your cell
+        $pdf->Cell(95, 8, "Rs. " . number_format($discounts[0]->salesreturn, 2), 0, 1, 'R', 1); 
+       
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->setFillColor(230, 230, 230);
+        $pdf->SetTextColor(0, 0, 0);
+        $pdf->Cell(95, 8, "Salaries", 0, 0, 'L', 1); //your cell
+        $pdf->Cell(95, 8, "Rs. " . number_format($salaries[0]->salaries, 2), 0, 1, 'R', 1); 
 
        
 
-        $pdf->Cell(70, 6, 'Discounts', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, number_format($discounts[0]->discounts, 2), 0, 1, 'R');
+        // $pdf->Cell(70, 6, 'Discounts', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, number_format($discounts[0]->discounts, 2), 0, 1, 'R');
 
-        $pdf->Cell(70, 6, 'Sales Return', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, number_format($salesreturn[0]->salesreturn, 2), 0, 1, 'R');
+        // $pdf->Cell(70, 6, 'Sales Return', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, number_format($salesreturn[0]->salesreturn, 2), 0, 1, 'R');
 
 
 
-        $pdf->Cell(70, 6, 'Salaries', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, '', 0, 0, 'L');
-        $pdf->Cell(40, 6, number_format($salaries[0]->salaries, 2), 0, 1, 'R');
+        // $pdf->Cell(70, 6, 'Salaries', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, '', 0, 0, 'L');
+        // $pdf->Cell(40, 6, number_format($salaries[0]->salaries, 2), 0, 1, 'R');
 
-        $expesnsetotal = ($expesnsetotal  +  $discounts[0]->discounts + $salesreturn[0]->salesreturn + $salaries[0]->salaries); //$purchases[0]->purchase_amount
+        // $expesnsetotal = ($expesnsetotal  +  $discounts[0]->discounts + $salesreturn[0]->salesreturn + $salaries[0]->salaries); //$purchases[0]->purchase_amount
 
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(95, 5, 'Total Expenses', 0, 0, 'L');
-        $pdf->Cell(95, 5, number_format($expesnsetotal, 2), 0, 1, 'R');
+        // $pdf->SetFont('Arial', 'B', 12);
+        // $pdf->Cell(95, 5, 'Total Expenses', 0, 0, 'L');
+        // $pdf->Cell(95, 5, number_format($expesnsetotal, 2), 0, 1, 'R');
 
         //Expense END HERE
         $pdf->Cell(190, 2, '', '', 1); //SPACE
