a72cc4ffcbc732cfd442d5348286490e285c2fc5
uzairrafiq <uzairrafiq_@hotmail.com>
2025-06-12 18:48:02 +0500

    working

--- routes/website.php
+++ routes/website.php
@@ -84,8 +84,8 @@ Route::middleware(['statusCheck'])->group(function () {
 
     Route::resource('website', WebsiteController::class)->except(['show']);
     Route::get('website/{mode?}', [WebsiteController::class, 'index'])->name('inactiveWebsitelists');
-    Route::get('website/{id}', function () {
-       return redirect()->route('inactiveWebsitelists'); // ya koi bhi custom route
-    });
+    // Route::get('website/{id}', function () {
+    //    return redirect()->route('inactiveWebsitelists'); // ya koi bhi custom route
+    // });
     /******************************* website panel route closing **********************************/
 });
